001995f42557585674566135b2e94b8cea2e33f2
Peco Danajlovski <pecod@sourcico.com>
2022-06-20 17:48:07 +0200

    removed enums and using structs for defining and running commands

--- src/main.rs
+++ src/main.rs
@@ -4,40 +4,80 @@ fn main() {
     const VERSION: &str = env!("CARGO_PKG_VERSION");
     let args: Vec<String> = env::args().collect();
 
-    let command = &args[1];
+    let command_arg: String = args[1].clone();
 
-    #[derive(Debug)]
-    enum Command {
-        Help,
-        ToOwnFolder,
-        Version,
-        Unrecognized
-    }
+    // Generate the commands here
+    let mut commands: Vec<Command> = vec![];
 
-    let chosen = match command.as_str() {
-        "help" => Command::Help,
-        "toownfolder" => Command::ToOwnFolder,
-        "version" => Command::Version,
-        _ => Command::Unrecognized
-    };
+    commands.push(Command::new("help", "Help"));
+    commands.push(Command::new("toownfolder", "To own folder"));
+    commands.push(Command::new("version", "Version"));
 
-    println!("You have chosen the command: {:?}", chosen);
-
-    match chosen {
-        Command::ToOwnFolder => {
-            println!("Implement stuff here")
+    match command_arg.as_str() {
+        "help" => {
+            println!("List of available commands:");
+            for command in commands {
+                println!("{}", command.display_description());
+            }
         }
-        Command::Help => {
-            println!("List all commands here")
-        },
-        Command::Version => {
+        "version" => {
             println!("Grunty-Rust v{}", VERSION)
         }
-        Command::Unrecognized => {
-            println!("Command unrecognized")
+        _ => {
+            let commands = get_command_by_name(commands, command_arg);
+            for cmd in commands {
+                let result = cmd.run();
+                println!("Running command: {}", result);
+            }
+        }
+    };
+}
+
+#[derive(PartialEq, Debug)]
+struct Command {
+    name: String,
+    description: String,
+}
+
+impl Command {
+    fn new(name: &str, description: &str) -> Command {
+        Command {
+            name: String::from(name),
+            description: String::from(description),
         }
     }
 
+    fn display_description(&self) -> String {
+        String::from(&self.description)
+    }
+
+    fn run(&self) -> String {
+        return String::from(&self.name);
+    }
 }
 
+fn get_command_by_name(commands: Vec<Command>, command_arg: String) -> Vec<Command> {
+    return commands
+        .into_iter()
+        .filter(|item| item.name == command_arg)
+        .collect::<Vec<Command>>();
+}
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn command_by_name() {
+        let cmd1 = Command::new("one", "First");
+        let cmd2 = Command::new("two", "Second");
+        let cmd3 = Command::new("three", "Third");
+
+        let cmds = vec![cmd1, cmd2, cmd3];
+
+        assert_eq!(
+            vec![Command::new("two", "Second")],
+            get_command_by_name(cmds, String::from("two"))
+        );
+    }
+}
